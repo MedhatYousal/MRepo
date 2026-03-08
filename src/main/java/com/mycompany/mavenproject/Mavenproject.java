c11edadb7634cb4e314eb431d14040aab53b29ed
Medhat.Yousal <medhat.yousal@gmail.com>
2026-03-08 14:50:45 +0200

    Changes 7-Mar-2026

---
+++ src/main/java/com/mycompany/mavenproject/Mavenproject.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author Medhat
+ */
+public class Mavenproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
